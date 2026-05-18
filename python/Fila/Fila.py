089d91a2adb2234bf9995ddae128752e2838c3e7
jaldsonarthur17 <jaldsonarthur1322@gmail.com>
2026-05-18 15:22:39 -0300

    finalização da classe Fila e funções

--- python/Fila/Fila.py
+++ python/Fila/Fila.py
@@ -14,12 +14,22 @@ class Fila:
         if len(self._dados) == 0:
             return None
         return self._dados[0]
+    
+    def tamanho_da_fila(self):
+        return len(self._dados)
+    
+    def listar_na_fila(self):
+        return self._dados
         
 fila1 = Fila()
 fila1.enfileirar({"nome": "Ana"})
 fila1.enfileirar({"nome": "Pedro"})
+fila1.enfileirar({"nome": "Carlos"})
 
-print(fila1.ir_pra_frente())   # → {'nome': 'Ana'}
+print(fila1.tamanho_da_fila())
+print(fila1.listar_na_fila())
+
+print(fila1.ir_pra_frente())   
 print(fila1._dados)  
 
 removido = fila1.remover_da_fila()
