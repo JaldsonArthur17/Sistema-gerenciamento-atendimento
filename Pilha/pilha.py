276db9c7ed36d31cf980d89ca05beb74c724f82a
jaldsonarthur17 <jaldsonarthur1322@gmail.com>
2026-05-25 16:26:41 -0300

    feat: finalização da pilha e métodos

--- Pilha/pilha.py
+++ Pilha/pilha.py
@@ -10,6 +10,14 @@ class Pilha:
             return None
         return self._dados.pop()
     
+    def topo(self):
+        if len(self._dados) == 0:
+            return None
+        return self._dados[-1]
+    
+    def tamanho(self):
+        return len(self._dados)
+    
 pilha = Pilha()
 pilha.empilhar({"nome": "Ana",   "hora": "09:00"})
 pilha.empilhar({"nome": "Pedro", "hora": "09:15"})
@@ -18,5 +26,7 @@ pilha.empilhar({"nome": "Maria", "hora": "09:30"})
 print(pilha.desempilhar())
 print(pilha.desempilhar())
 print(pilha._dados)
+print(pilha.topo())
+print(pilha.tamanho())
 
 print(pilha._dados)
